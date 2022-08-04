aa0d95a8b3c2e306c21cc956d59f5dc2a7505e10
Sergey Seleznev <seleznevsergey2012@gmail.com>
2022-08-04 18:18:21 +0300

    solution

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -2,17 +2,20 @@ package hiber.dao;
 
 import hiber.model.User;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+   private final SessionFactory sessionFactory;
+
+   public UserDaoImp(SessionFactory sessionFactory) {
+      this.sessionFactory = sessionFactory;
+   }
 
    @Override
    public void add(User user) {
@@ -25,5 +28,14 @@ public class UserDaoImp implements UserDao {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
+
+   public User getUserByCar(String model, int series){
+
+      Query query = sessionFactory.getCurrentSession().createQuery("from User user where user.car.series = :series and user.car.model = :model");
+      query.setParameter("model", model).setParameter("series", series);
+      return (User) query.setMaxResults(1).getSingleResult();
+
+   }
+
 
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -1,61 +1,97 @@
 package hiber.model;
 
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
 public class User {
 
-   @Id
-   @GeneratedValue(strategy = GenerationType.IDENTITY)
-   private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
-   @Column(name = "name")
-   private String firstName;
+    private long id;
 
-   @Column(name = "last_name")
-   private String lastName;
+    @Column(name = "name")
+    private String firstName;
 
-   @Column(name = "email")
-   private String email;
+    @Column(name = "last_name")
+    private String lastName;
 
-   public User() {}
-   
-   public User(String firstName, String lastName, String email) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-      this.email = email;
-   }
+    @Column(name = "email")
+    private String email;
 
-   public Long getId() {
-      return id;
-   }
 
-   public void setId(Long id) {
-      this.id = id;
-   }
+    @OneToOne
+    @Cascade(org.hibernate.annotations.CascadeType.ALL)
+    @JoinColumn(name = "car_id")
+    private Car car;
 
-   public String getFirstName() {
-      return firstName;
-   }
+    public User() {
+    }
 
-   public void setFirstName(String firstName) {
-      this.firstName = firstName;
-   }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", car=" + car +
+                '}';
+    }
 
-   public String getLastName() {
-      return lastName;
-   }
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
 
-   public void setLastName(String lastName) {
-      this.lastName = lastName;
-   }
+    public User(Car car, String firstName, String lastName, String email) {
+        this(firstName, lastName, email);
+        this.car = car;
 
-   public String getEmail() {
-      return email;
-   }
+    }
 
-   public void setEmail(String email) {
-      this.email = email;
-   }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
